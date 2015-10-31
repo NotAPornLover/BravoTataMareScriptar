29f439e2348fa95f526cda646c3bc6d817173c76
NotAPornLover <huibanrares26@gmail.com>
2015-10-31 17:59:32 +0200

    MareMort
    Only For Romanian Players

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,19 @@
+﻿using LeagueSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Game.PrintChat("Hello World!");
+
+
+        }
+    }
+}
